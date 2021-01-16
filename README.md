f466019be6ffff0b91264c72f52e514c92f3074d
Vikram Sharma <vikram.sharma1024@gmail.com>
2021-01-16 14:45:32 -0500

    readme, fix profile update error

---
+++ README.md
@@ -0,0 +1,19 @@
+# Full Stack Banking Application
+
+Source code: https://github.com/myogeshchavan97/fullstack_banking_app/tree/section-1-using-class-components
+
+Originally built by Yogesh Chavan, [Tutorial Part 1](https://medium.com/javascript-in-plain-english/create-a-fullstack-banking-application-using-react-e8c96d74cd39)
+
+###For more of my work, visit my portfolio [here](https://www.vikramraisharma.dev/)!
+
+##To view the application locally:
+1. Clone repo and run `yarn install`
+2. `cd` into `server` and run `yarn install`
+3. Run `yarn start` in both `src` and `server` folders
+4. Open [localhost](http://localhost:3000) to view the React App
+
+##Key learning areas:
+- PERN stack (PostgreSQL, Express, React/Redux, Node)
+- Using Axios to exchange information between Node API and React app
+- Basics of JWT and using authorization tokens/middleware
+
--- fullstack-banking-app/server/routes/profile.js
+++ fullstack-banking-app/server/routes/profile.js
@@ -1,28 +1,32 @@
 const express = require('express');
 const authMiddleware = require('../middleware/auth')
 const {pool} = require('../db/connect')
-const {isInavlidField} = require('../utils/common');
+const {isInvalidField} = require('../utils/common');
 const Router = express.Router()
 
 Router.post('/profile', authMiddleware, async (req, res) => {
     try {
+        // grab name fields to update, check if they are valid
         const {first_name, last_name} = req.body;
         const validFieldsToUpdate = ['first_name', 'last_name'];
         const receivedFields = Object.keys(req.body);
-
-        const isInavlidFieldProvided = isInavlidField(
+        console.log('receivedFields: ', receivedFields);
+        const isInvalidFieldProvided = isInvalidField(
             receivedFields,
             validFieldsToUpdate
         )
-        if(isInavlidFieldProvided){
+        if(isInvalidFieldProvided){
             return res.status(400).send({
                 update_error: 'Invalid field.'
             })
         }
+        console.log('query reached, params: ', first_name, last_name, req.user.userid);
+        // query psql table to update the user with the given params
         const result = await pool.query(
-            'update bank_user set first_name=$1, last_name=$2, where userid=$3 returning userid, first_name, last_name, email',
+            'update bank_user set first_name=$1, last_name=$2 where userid=$3 returning userid, first_name, last_name, email;',
             [first_name, last_name, req.user.userid]
         )
+        
         res.send(result.rows[0]);
     } catch (error) {
         res.status(400).send({
